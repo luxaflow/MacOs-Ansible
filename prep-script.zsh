e16d96c588762c37890397664f83c9596c7359ee
Lucas Wolfe <lucaswolfe@lucass-mbp.home>
2022-01-07 08:37:29 +0100

    added it alias for new repo push

--- prep-script.zsh
+++ prep-script.zsh
@@ -2,4 +2,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install git ansible
 git config --global alias.add-commit '!git add -A && git commit'
+git config --global alias.push-up '!git push -u origin head'
 ansible-playbook main.yml -K
